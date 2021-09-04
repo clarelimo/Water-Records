4ea18370b836fcb79daffbb89c59da8fd7ae8a1f
clarelimo <clare.limo@student.moringaschool.com>
2021-09-04 19:01:42 +0300

    pass data between activities

--- app/src/main/java/com/moringaschool/waterrefillrecords/SalesActivity.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/SalesActivity.java
@@ -2,15 +2,22 @@ package com.moringaschool.waterrefillrecords;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SalesActivity extends AppCompatActivity {
+    @BindView(R.id.shopNameTextView)TextView mShopNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
-        String location = intent.getStringExtra("shop name");
+        String shop_name = intent.getStringExtra("shop name");
+        mShopNameTextView.setText("This Week's Sales for: " + shop_name+ " Shop");
     }
 }
