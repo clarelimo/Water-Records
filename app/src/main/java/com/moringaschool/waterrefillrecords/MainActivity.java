bb4f2c8e1644aa58e3521058399046010acca5ef
clarelimo <clare.limo@student.moringaschool.com>
2021-09-04 18:33:44 +0300

    butterknife

--- app/src/main/java/com/moringaschool/waterrefillrecords/MainActivity.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/MainActivity.java
@@ -3,12 +3,20 @@ package com.moringaschool.waterrefillrecords;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.getStartedButton) Button mFindRestaurantsButton;
+    @BindView(R.id.shopNameEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
