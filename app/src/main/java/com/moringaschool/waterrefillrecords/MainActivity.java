c726b2fce70af82885e04dd3c8b39af8534a1ef2
clarelimo <clare.limo@student.moringaschool.com>
2021-09-04 18:39:01 +0300

    get shop name

--- app/src/main/java/com/moringaschool/waterrefillrecords/MainActivity.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/MainActivity.java
@@ -2,14 +2,19 @@ package com.moringaschool.waterrefillrecords;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.getStartedButton) Button mFindRestaurantsButton;
-    @BindView(R.id.shopNameEditText) EditText mLocationEditText;
+    @BindView(R.id.getStartedButton) Button mGetStartedButton;
+    @BindView(R.id.shopNameEditText) EditText mShopNameEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
 
@@ -17,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        mGetStartedButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String location = mShopNameEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SalesActivity.class);
+                intent.putExtra("shop name", location);
+                startActivity(intent);
+            }
+        });
     }
 }
