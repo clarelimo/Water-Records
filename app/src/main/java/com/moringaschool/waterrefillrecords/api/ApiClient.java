37cffb4502da3e0d214159734ef9a1dc759cba7e
clarelimo <clare.limo@student.moringaschool.com>
2021-09-15 10:43:52 +0300

    base url added

---
+++ app/src/main/java/com/moringaschool/waterrefillrecords/api/ApiClient.java
@@ -0,0 +1,12 @@
+package com.moringaschool.waterrefillrecords.api;
+
+import retrofit2.Retrofit;
+
+public class ApiClient {
+    private static Retrofit retrofit = null;
+
+    public static final String BASE_URL = "https://10.0.2.2:4567";
+//    public static ApiInterface getClient(){
+//
+//    }
+}
