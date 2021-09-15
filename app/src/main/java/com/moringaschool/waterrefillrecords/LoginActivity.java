8b72aeb603fce42e0ba3bc9da38dc34fe3466d6d
clarelimo <clare.limo@student.moringaschool.com>
2021-09-15 09:50:24 +0300

    added intent to connect to sales

--- app/src/main/java/com/moringaschool/waterrefillrecords/LoginActivity.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/LoginActivity.java
@@ -2,13 +2,37 @@ package com.moringaschool.waterrefillrecords;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.loginButton) Button mLoginButton;
+    @SuppressLint("NonConstantResourceId")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            Intent intent = new Intent(LoginActivity.this, SalesActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
 }
