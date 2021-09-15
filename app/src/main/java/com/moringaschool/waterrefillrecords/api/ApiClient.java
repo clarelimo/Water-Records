ea33086f7a5a918f1dafee387381fb07636c27fa
clarelimo <clare.limo@student.moringaschool.com>
2021-09-15 14:00:23 +0300

    create a retrofit instance

--- app/src/main/java/com/moringaschool/waterrefillrecords/api/ApiClient.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/api/ApiClient.java
@@ -1,12 +1,19 @@
 package com.moringaschool.waterrefillrecords.api;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static Retrofit retrofit = null;
 
     public static final String BASE_URL = "https://10.0.2.2:4567";
-//    public static ApiInterface getClient(){
-//
-//    }
+    public static ApiInterface getClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiInterface.class);
+    }
 }
