8f2d8e1db40eb919802ab619ea22611a51c26b37
clarelimo <clare.limo@student.moringaschool.com>
2021-09-15 10:30:17 +0300

    added sales api interface

---
+++ app/src/main/java/com/moringaschool/waterrefillrecords/api/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.moringaschool.waterrefillrecords.api;
+
+import com.moringaschool.waterrefillrecords.SalesActivity;
+import com.moringaschool.waterrefillrecords.modules.Expenses;
+import com.moringaschool.waterrefillrecords.modules.Sales;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("sales")
+    Call<List<Sales>> getSales();
+
+
+}
