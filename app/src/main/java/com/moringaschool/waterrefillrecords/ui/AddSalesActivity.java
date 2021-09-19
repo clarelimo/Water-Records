1f77a2d39a6c8b367473ca1fc2ef55ad3662d39e
clarelimo <clare.limo@student.moringaschool.com>
2021-09-19 13:42:25 +0300

    add sales alyout complete

---
+++ app/src/main/java/com/moringaschool/waterrefillrecords/ui/AddSalesActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.waterrefillrecords.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.waterrefillrecords.R;
+
+public class AddSalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sales);
+    }
+}
---
+++ app/src/main/java/com/moringaschool/waterrefillrecords/ui/SaleDetailActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.waterrefillrecords.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.waterrefillrecords.R;
+
+public class SaleDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_detail);
+    }
+}
