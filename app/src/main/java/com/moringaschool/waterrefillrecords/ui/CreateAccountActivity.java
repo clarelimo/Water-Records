3dee2639e48f855644823381003370f59419cf87
clarelimo <clare.limo@student.moringaschool.com>
2021-09-19 08:07:48 +0300

    create account activity and layout created

---
+++ app/src/main/java/com/moringaschool/waterrefillrecords/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.waterrefillrecords.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.waterrefillrecords.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
