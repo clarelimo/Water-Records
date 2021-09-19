cd1412b8f00c2f1bad8243b9c13451bcce4be87f
clarelimo <clare.limo@student.moringaschool.com>
2021-09-19 10:02:39 +0300

    logout menu added and inflated

--- app/src/main/java/com/moringaschool/waterrefillrecords/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/waterrefillrecords/ui/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,4 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
